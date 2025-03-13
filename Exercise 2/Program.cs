5ad4961125eb12f87f9926bbd754699acf57b549
Daniel Page <dpage105@gmail.com>
2025-03-12 23:28:56 -0500

    Completed Exercise 2 by creating a switch statement to process useer input reagarding favorite school subject.

--- Exercise 2/Program.cs
+++ Exercise 2/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        Console.WriteLine("What is your favorite school subject?");
+        string favoriteSubject = Console.ReadLine();
+
+        switch (favoriteSubject)
+        {
+            case "Math":
+                Console.WriteLine("Great choice.  Math is a very useful subject and has a wide variety of applications.");
+                break;
+            case "Programming":
+                Console.WriteLine("Being able to write computer programs will open many doors in the modern job market.");
+                break;
+            case "English":
+                Console.WriteLine("Mastery of the English language will allow you to become an effective communicator.");
+                break;
+            case "Physics":
+                Console.WriteLine("Studying the laws of physics will give tremendous insight into some of the world's greatest mysteries.");
+                break;
+            case "Spanish":
+                Console.WriteLine("Being fluent in multiple languages will give you an advantage when speaking with people of other cultures.");
+                break;
+            case "History":
+                Console.WriteLine("The study of history is fascinating and will help you better understand the nature of humanity.");
+                break;
+            default:
+                Console.WriteLine("That is a very unique and interesting choice.  What is your favorite part about this subject?");
+                break;
+        }
     }
 }
--- Exercise 2/obj/Debug/net9.0/Exercise 2.AssemblyInfo.cs
+++ Exercise 2/obj/Debug/net9.0/Exercise 2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+209b563aa0b047ba69e44a4800b13a50a08d890f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f15ddf3f637b5984c701878ef48e537e5891d077")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
